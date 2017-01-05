764d84fe91be0ae4d15538a064ecb04ff410eda9
Bobby Yang <bobstermyang@users.noreply.github.com>
2017-01-04 19:46:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ for (startNumber = 1; startNumber < 100; startNumber++) {
 
   else if (startNumber%5 === 0) {
     document.write(buzz+"<br>");
+	  //this it bs
+	  //youdont know
+	  document.write(buzz+"br">);
   }
 
   else if (startNumber%3 === 0) {
